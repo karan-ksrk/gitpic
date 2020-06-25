4894f4b58ab1ebc64b3745d49e0fa1e40395e78d
karan-ksrk <karanksrk2000@outlook.com>
2020-06-25 23:25:43 +0530

    Updates

---
+++ precache-manifest.bc1ba3496667368a3b1861e25ac81f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662961dd31f59349bd3bc9ebe8d88002",
+    "url": "/gitpic/index.html"
+  },
+  {
+    "revision": "d869d576482e88f41ba6",
+    "url": "/gitpic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01a427725f3beb4608a5",
+    "url": "/gitpic/static/js/2.862b0ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitpic/static/js/2.862b0ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d869d576482e88f41ba6",
+    "url": "/gitpic/static/js/main.566814ab.chunk.js"
+  },
+  {
+    "revision": "745e2099d269c3c7abb9",
+    "url": "/gitpic/static/js/runtime-main.db977b1f.js"
+  }
+]);
